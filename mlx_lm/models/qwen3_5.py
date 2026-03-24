8e94256220f954949133e036980951681e353945
Ryuichi Leo Takashige <leo@exolabs.net>
2026-03-24 15:30:25 +0000

    Cleanup

--- mlx_lm/models/qwen3_5.py
+++ mlx_lm/models/qwen3_5.py
@@ -188,7 +188,7 @@ class GatedDeltaNet(nn.Module):
             self.dt_bias,
             state,
             mask,
-            use_kernel=True,
+            use_kernel=not self.training,
         )
 
         if cache is not None:
